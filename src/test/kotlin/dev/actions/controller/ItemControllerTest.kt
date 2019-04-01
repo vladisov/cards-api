ae724c07886baa5c0c0dfe0b6dfd77351675902b
vladisov <vlad079@ya.ru>
2019-04-01 23:14:52 +0200

    adjusting tests

--- src/test/kotlin/dev/actions/controller/ItemControllerTest.kt
+++ src/test/kotlin/dev/actions/controller/ItemControllerTest.kt
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.http.MediaType
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.FluxExchangeResult
@@ -41,11 +42,15 @@ class ItemControllerTest {
     private lateinit var item: Item
     private lateinit var itemJson: String
 
+    private val validToken = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjpbIlJPTEVfVVNFUiJdLCJzdWIiOiJ1c2VyIiwiaWF0IjoxNTU0MTQ1OTA0LCJleHAiOjkyNDY0NTg1MjAwfQ.9phRt48Qjsqgf12907wsL8HPSyoRe4HZLk2iWwnODmviXELGhAGsY6ltCaT89vqygildXOCXA37mxVNKq8tnTA"
+
+
 
     @BeforeEach
     fun setup() {
         webClient = WebTestClient
                 .bindToController(ItemController(itemRepository))
+                .apply<WebTestClient.ControllerSpec>(SecurityMockServerConfigurers.springSecurity())
                 .build()
 
         item = Item("123", "desc", "res", LocalDateTime.of(2019, 1, 1, 0, 0, 0), "userId")
@@ -53,14 +58,15 @@ class ItemControllerTest {
         val itemFlux = Flux.just(item)
 
         given(itemRepository.save(item)).willReturn(Mono.just(item))
-        given(itemRepository.findAll()).willReturn(itemFlux)
-        given(itemRepository.findByDescriptionContaining("desc")).willReturn(itemFlux)
-        given(itemRepository.findByResultContaining("res")).willReturn(itemFlux)
+        given(itemRepository.findAllByUsername("user")).willReturn(itemFlux)
+        given(itemRepository.findByDescriptionContainingAndUsername("desc", "user")).willReturn(itemFlux)
+        given(itemRepository.findByResultContainingAndUsername("res", "user")).willReturn(itemFlux)
     }
 
     @Test
     fun testGetAllItemsSuccess() {
         val result: FluxExchangeResult<Item> = webClient.get().uri("/api/item")
+                .header(validToken)
                 .exchange()
                 .expectStatus().isOk
                 .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
