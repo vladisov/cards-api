dbf0d1124ca0d0035d429465b053649749cb10c1
gslavov <georgi.slavov@nl.abnamro.com>
2020-01-24 14:36:29 +0100

    Add Jasypt support

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:alpine
 EXPOSE 8082
 ADD ./target/cards-1.0-SNAPSHOT.jar cards-api.jar
-ENTRYPOINT ["java", "-jar", "cards-api.jar"]
+ENTRYPOINT ["java", "-jar", "-Djasypt.encryptor.password=SNATCH-PASs_WORD", "cards-api.jar"]
--- src/main/kotlin/dev/cards/CardsApplication.kt
+++ src/main/kotlin/dev/cards/CardsApplication.kt
@@ -1,9 +1,11 @@
 package dev.cards
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableEncryptableProperties
 class CardsApplication
 
 fun main(args: Array<String>) {
