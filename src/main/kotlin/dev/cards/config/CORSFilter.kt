c381a52486ec5e7e24219776f4103ce8aa70ba91
vladisov <vlad079@ya.ru>
2020-02-02 09:45:58 +0100

    applied cors filter

--- src/main/kotlin/dev/cards/config/CORSFilter.kt
+++ src/main/kotlin/dev/cards/config/CORSFilter.kt
@@ -1,12 +1,16 @@
 package dev.cards.config
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 
 /**
  * @author vladov 2019-03-31
  */
+@Configuration
+@EnableWebFlux
 class CORSFilter : WebFluxConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
